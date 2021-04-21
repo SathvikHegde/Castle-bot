d6839bbe08996f262ec83d313f0b6ead32fdac1d
SathvikHegde <62826093+SathvikHegde@users.noreply.github.com>
2021-04-21 22:35:08 +0530

    ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const cleverbot = require("cleverbot-free");
+const cleverbot = require("cleverbotnot");
 
 client.once('ready', () => {
     console.log('Bot is online!');
