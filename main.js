d84fcac159245e4b2fcb1d6e9b8402e1874d8f21
SathvikHegde <62826093+SathvikHegde@users.noreply.github.com>
2021-07-12 22:12:40 +0530

    added feature to see others points

--- main.js
+++ main.js
@@ -65,9 +65,79 @@ client.on('message', async message =>{
     }
 
     if(message.channel.id == '799987917065682964' && message.content == '-weeklything'){
-        let embed = new Discord.MessageEmbed()
-        .setTitle("**Weekly Leaderboard**");
+      calcleaderboard();
+      embed.setTitle("**Weekly Leaderboard**");
+      client.channels.cache.get("835225151787892766").send(embed);
+      client.channels.cache.get("835225151787892766").send('https://static.wikia.nocookie.net/webtoon/images/b/bf/Blades_of_Furry_Banner_3.gif');
+    }
 
+    if(message.channel.id == '799987917065682964' && message.content == '-resetlevels'){
+      level.deleteMany({ serverID: '799971756772818964' }, function (err) {
+        if(err) console.log(err);
+        console.log("Successful deletion");
+        message.reply('Sucessfully rested the database.');
+      });
+    }  
+
+    if(command == 'bonk'){
+      let bonker = message.author;
+      let target = message.mentions.users.first() || message.author;
+      let randomselect = Math.floor((Math.random() * 7) + 1);
+      let gif;
+      
+      if(randomselect == 1){
+        gif = 'https://media1.tenor.com/images/347f852d3dfa48502406fa949fcc1449/tenor.gif?itemid=15150394';
+      }else if(randomselect == 2){
+        gif = 'https://media1.tenor.com/images/c6571c335cd8e56de03ae05f81790efa/tenor.gif?itemid=20294899';
+      }else if(randomselect == 3){
+        gif = 'https://media1.tenor.com/images/79e0ed5c2ed5397fa79f48fccd6265d1/tenor.gif?itemid=20952854';
+      }else if(randomselect == 4){
+        gif = 'https://media1.tenor.com/images/dc4329d27745a6707219cb658f5b2c46/tenor.gif?itemid=18191826';
+      }else if(randomselect == 5){
+        gif = 'https://media1.tenor.com/images/119ca32322ba24e4ffc4f0d84a6839f1/tenor.gif?itemid=17402810';
+      }else if(randomselect == 6){
+        gif = 'https://media1.tenor.com/images/8cd54ee389b04a2366e85332125c5475/tenor.gif?itemid=20920340';
+      }else if(randomselect == 7){
+        gif = 'https://media1.tenor.com/images/cf9f90ce4ccca4fe6d82bb445ca4759e/tenor.gif?itemid=8229175';
+      }
+
+      const embed = new Discord.MessageEmbed()
+      .setDescription(`<@${bonker.id}> bonked <@${target.id}>`)
+      .setImage(gif);
+
+      message.channel.send(embed);
+    }
+
+    if(command == 'leaderboard' || command == 'lb') {
+      calcleaderboard();
+      embed.setTitle("**Current Weekly Leaderboard**");
+      message.channel.send(embed);
+    }
+
+    if(command == 'level' || command == 'lvl') {
+      let target = message.mentions.users.first() || message.author;
+      level.findOne({
+        userID: target.id,
+      }, (err, res) => {
+        if(err) console.log(err);
+        if(!res){
+          message.channel.send(`Hey! It looks like ${target.username} hasn't talked in this server before.`)
+        }else{
+          const embed = new Discord.MessageEmbed()
+          .setDescription(`**${target.username}'s** has ${res.points} points.`)
+          message.channel.send(embed);
+        }
+      })
+    }
+ 
+    
+
+});
+
+client.login(process.env.token);
+
+function calcleaderboard() {
+  let embed = new Discord.MessageEmbed()
         level.find({
             serverID: message.guild.id
           }).sort([
@@ -119,52 +189,5 @@ client.on('message', async message =>{
               }
             }
             embed.setTimestamp();
-
-            client.channels.cache.get("835225151787892766").send(embed);
-            client.channels.cache.get("835225151787892766").send('https://static.wikia.nocookie.net/webtoon/images/b/bf/Blades_of_Furry_Banner_3.gif');
-        
           })
-    }
-
-    if(message.channel.id == '799987917065682964' && message.content == '-resetlevels'){
-      level.deleteMany({ serverID: '799971756772818964' }, function (err) {
-        if(err) console.log(err);
-        console.log("Successful deletion");
-        message.reply('Sucessfully rested the database.');
-      });
-    }  
-
-    if(command == 'bonk'){
-      let bonker = message.author;
-      let target = message.mentions.users.first() || message.author;
-      let randomselect = Math.floor((Math.random() * 7) + 1);
-      let gif;
-      
-      if(randomselect == 1){
-        gif = 'https://media1.tenor.com/images/347f852d3dfa48502406fa949fcc1449/tenor.gif?itemid=15150394';
-      }else if(randomselect == 2){
-        gif = 'https://media1.tenor.com/images/c6571c335cd8e56de03ae05f81790efa/tenor.gif?itemid=20294899';
-      }else if(randomselect == 3){
-        gif = 'https://media1.tenor.com/images/79e0ed5c2ed5397fa79f48fccd6265d1/tenor.gif?itemid=20952854';
-      }else if(randomselect == 4){
-        gif = 'https://media1.tenor.com/images/dc4329d27745a6707219cb658f5b2c46/tenor.gif?itemid=18191826';
-      }else if(randomselect == 5){
-        gif = 'https://media1.tenor.com/images/119ca32322ba24e4ffc4f0d84a6839f1/tenor.gif?itemid=17402810';
-      }else if(randomselect == 6){
-        gif = 'https://media1.tenor.com/images/8cd54ee389b04a2366e85332125c5475/tenor.gif?itemid=20920340';
-      }else if(randomselect == 7){
-        gif = 'https://media1.tenor.com/images/cf9f90ce4ccca4fe6d82bb445ca4759e/tenor.gif?itemid=8229175';
-      }
-
-      const embed = new Discord.MessageEmbed()
-      .setDescription(`<@${bonker.id}> bonked <@${target.id}>`)
-      .setImage(gif);
-
-      message.channel.send(embed);
-    }
- 
-    
-
-});
-
-client.login(process.env.token);
+}
