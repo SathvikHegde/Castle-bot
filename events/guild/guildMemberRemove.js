6f7447ee1d0f5fee5bcda0da7bdb710fd21d384e
SathvikHegde <62826093+SathvikHegde@users.noreply.github.com>
2021-09-09 09:48:08 +0530

    Changed the channel of goodbye message

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,4 +1,4 @@
 module.exports = (Discord, client, member) =>{
     if(member.guild.id != '799971756772818964') return;
-    client.channels.cache.get('843041767216906282').send(`Oh no! <@${member.id}> (${member.displayName}) is leaving us. We hope you come back soon!`);
+    client.channels.cache.get('818069197598359602').send(`Oh no! <@${member.id}> (${member.displayName}) is leaving us. We hope you come back soon!`);
 }
